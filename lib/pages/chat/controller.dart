9251a6c244e06088dfe3967c47ba0d1d80157da9
dollarkillerx <adapawang@gmail.com>
2023-03-07 22:24:15 +0800

    [UDP] search contacts

--- lib/pages/chat/controller.dart
+++ lib/pages/chat/controller.dart
@@ -2,6 +2,6 @@ import 'package:get/get.dart';
 import '../../models/users.dart';
 
 class ChatController extends GetxController {
-  Friendship friendship = Get.arguments['friendship'];
+  UserInfo friendship = Get.arguments['friendship'];
 
 }
